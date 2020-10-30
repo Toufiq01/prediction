2bbd55f5ce1999d9308e25bb027f6f4e90ed0c8f
Toufiq01 <mdsoumonaziz@gmail.com>
2020-10-30 22:31:44 +0600

    Update eur usd forcast.py

--- eur usd forcast.py
+++ eur usd forcast.py
@@ -1,3 +1,10 @@
+# import quandl, pandas, numpy and all other module need to done my job.
+#We'll be using the numpy module to convert data to numpy arrays,
+#which is what Scikit-learn wants. We will talk more on preprocessing and cross_validation 
+#when we get to them in the code, but preprocessing is the module used to do some cleaning/scaling of data prior to machine learning, and 
+#cross_ alidation is used in the testing stages.
+#Finally, we're also importing the LinearRegression algorithm as well as svm from Scikit-learn,
+#which we'll be using as our machine learning algorithms to demonstrate results.
 import pandas as pd
 import numpy as np
 import quandl
@@ -30,8 +37,7 @@ df = df[['open','high','low','close','volume']]
 
 print(df.head())
 
-
-forecast_col = 'open'
+#Here, we define the forecasting column, then we fill any NaN data with -99999. 
 df.fillna(value=-99999, inplace=True)
 forecast_out = int(math.ceil(0.01 * len(df)))
 
@@ -41,9 +47,11 @@ df['label'] = df[forecast_col].shift(-forecast_out)
 
 df.dropna(inplace=True)
 
+
 X = np.array(df.drop(['label'], 1))
 y = np.array(df['label'])
 
+#it is included in the preprocessing module of Scikit-Learn. To utilize this applying preprocessing.scale to your X variable:
 X = preprocessing.scale(X)
 X_lately = X[-forecast_out:]
 y = np.array(df['label'])
@@ -53,7 +61,7 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_s
 
 from sklearn.ensemble import RandomForestRegressor
 
-
+#Scikit-Learn (sklearn),  train with .fit
 clf = LinearRegression()
 clf.fit(X_train, y_train)
 
@@ -61,13 +69,15 @@ accuracy = clf.score(X_test, y_test)
 y_pred = clf.predict(X_test)  
 
 print('accuracy ',accuracy)
-    
+#The forecast_set is an array of forecasts, showing that not only could you just seek out a single prediction  
 forecast_set = clf.predict(X_lately)
 print('forecast_set\n',forecast_set)
 
 
 df['Forecast'] = np.nan
 
+#recall that we predict 10% out into the future, and we saved that last 10% of our data to do this, 
+#thus, we can begin immediately predicting since -10% has data that we can predict 10% out and be the next prediction
 last_date = df.iloc[-1].name
 last_unix = last_date.timestamp()
 one_day = 3600
